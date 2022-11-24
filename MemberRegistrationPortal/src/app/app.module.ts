dd0fb13cc9515462e8724dd22fe5d0afdff630b2
unknown <cogjava3298@iiht.tech>
2022-11-24 11:19:22 +0530

    [#003] Created member registration component

--- MemberRegistrationPortal/src/app/app.module.ts
+++ MemberRegistrationPortal/src/app/app.module.ts
@@ -19,6 +19,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { MemberHomeComponent } from './member-home/member-home.component';
 
 
 
@@ -28,7 +29,8 @@ import { SignupComponent } from './signup/signup.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    MemberHomeComponent
   ],
   imports: [
     BrowserModule,
